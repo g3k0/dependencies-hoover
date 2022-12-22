203db1192e35dc5b2fa710228fcd76d91e91e991
Christian Palazzo <christian.palazzo@vimn.com>
2022-12-22 15:02:28 +0100

    added configuration file

---
+++ README.md
@@ -0,0 +1,6 @@
+# TO DO
+
+* write a config file that contains the ignore directories and files and a whitelist of dependencies, the file should be passed to the builded executable
+* integate in the code the logic to ignore directories and files and the depedencies to not delete (use regex)
+* implement the delete function
+* write tests
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{fs};
 use std::path::{Path};
 use serde_json::{Value, from_str};
 use std::collections::HashMap;
+use config::Config;
 
 mod helpers;
 
@@ -9,7 +10,20 @@ extern crate serde;
 extern crate serde_derive;
 
 fn main() {
-    let root_path: &Path = Path::new("/Users/palazzoc0905/Studio/oao-webplex/");
+
+    // Read config file
+
+    let settings_config: Config = Config::builder()
+        .add_source(config::File::with_name("config"))
+        .build()
+        .unwrap();
+
+    let settings:HashMap<String, String> = settings_config
+    .try_deserialize::<HashMap<String, String>>()
+    .unwrap();
+    let project_path: &str = settings.get("projectToScanPath").unwrap();
+
+    let root_path: &Path = Path::new(project_path);
     scan_directory(&root_path);
 }
 
