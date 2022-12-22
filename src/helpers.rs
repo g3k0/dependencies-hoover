de6fbe0be439c10c8608eb37ed7ec502e6671cbf
Christian Palazzo <christian.palazzo@vimn.com>
2022-12-22 17:03:48 +0100

    refactored

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,65 @@
 use std::path::{Path, PathBuf};
 use std::{fs};
+use serde_json::{Value, from_str};
+use std::collections::HashMap;
 
-pub fn is_dependency_used(dependency: &str, path: &Path) -> bool {
+extern crate serde;
+extern crate serde_derive;
+
+pub fn scan_directory(path: &Path) {
+    if path.is_dir() && !path.ends_with("node_modules") {
+
+        let Ok(_dir_entries): Result<fs::ReadDir, std::io::Error> = fs::read_dir(path) else { return };
+
+        for entry in fs::read_dir(path).unwrap() {
+
+            let entry_path: std::path::PathBuf = if let Ok(ref entry) = entry {
+                entry.path()
+            } else {
+                continue
+            };
+
+            let file_name: &str = entry_path.file_name().unwrap().to_str().unwrap();
+            if file_name == "node_modules" {
+                continue;
+            }
+
+            let entry_path: std::path::PathBuf = entry.unwrap().path();
+            scan_directory(&entry_path);
+        }
+    } else if path.file_name().unwrap() == "package.json" {
+        let file_contents: String = fs::read_to_string(path).unwrap();
+        let package_json: Value = from_str(&file_contents).unwrap();
+
+        if !package_json["dependencies"].is_null() {
+            let mut dependencies_map: HashMap<String, Value> = package_json["dependencies"].as_object().unwrap()
+            .iter()
+            .map(|(k, v)| (k.clone(), v.clone()))
+            .collect();
+
+            if !package_json["devDependencies"].is_null() {
+                let dev_dependencies_map: HashMap<String, Value> = package_json["devDependencies"].as_object().unwrap()
+                .iter()
+                .map(|(k, v)| (k.clone(), v.clone()))
+                .collect();
+
+                dependencies_map.extend(dev_dependencies_map);
+            }
+
+            for dependency in &dependencies_map {
+                let dep: &str = dependency.0.as_str();
+                if !is_dependency_used(&dep, &path.with_file_name("")) && 
+                   !file_exists_in_node_modules_bin(&dep, &path) {
+                    // delete_dependency(path, dependency);
+                    println!("{} | {}", path.display(), dep)
+                }
+            }
+        }
+    }
+}
+
+
+fn is_dependency_used(dependency: &str, path: &Path) -> bool {
     let mut usage = false;
     if path.is_dir() {
         let dir_name = path.file_name().unwrap().to_str().unwrap();
@@ -29,12 +87,12 @@ pub fn is_dependency_used(dependency: &str, path: &Path) -> bool {
     usage
 }
 
-pub fn file_exists_in_node_modules_bin(dependency: &str, path: &Path) -> bool {
+fn file_exists_in_node_modules_bin(dependency: &str, path: &Path) -> bool {
     let file_path: PathBuf = path.with_file_name("node_modules/.bin").join(dependency);
     file_path.is_file()
 }
 
-/* pub fn delete_dependency(package_json_path: &Path, dependency: &str) {
+/* fn delete_dependency(package_json_path: &Path, dependency: &str) {
     let file_contents = fs::read_to_string(package_json_path).unwrap();
     let mut package_json: PackageJson = serde_json::from_str(&file_contents).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::{fs};
-use std::path::{Path};
-use serde_json::{Value, from_str};
 use std::collections::HashMap;
 use config::Config;
+use std::path::{Path};
 
 mod helpers;
 
@@ -10,9 +8,7 @@ extern crate serde;
 extern crate serde_derive;
 
 fn main() {
-
     // Read config file
-
     let settings_config: Config = Config::builder()
         .add_source(config::File::with_name("config"))
         .build()
@@ -24,57 +20,5 @@ fn main() {
     let project_path: &str = settings.get("projectToScanPath").unwrap();
 
     let root_path: &Path = Path::new(project_path);
-    scan_directory(&root_path);
-}
-
-fn scan_directory(path: &Path) {
-    if path.is_dir() && !path.ends_with("node_modules") {
-
-        let Ok(_dir_entries): Result<fs::ReadDir, std::io::Error> = fs::read_dir(path) else { return };
-
-        for entry in fs::read_dir(path).unwrap() {
-
-            let entry_path: std::path::PathBuf = if let Ok(ref entry) = entry {
-                entry.path()
-            } else {
-                continue
-            };
-
-            let file_name: &str = entry_path.file_name().unwrap().to_str().unwrap();
-            if file_name == "node_modules" {
-                continue;
-            }
-
-            let entry_path: std::path::PathBuf = entry.unwrap().path();
-            scan_directory(&entry_path);
-        }
-    } else if path.file_name().unwrap() == "package.json" {
-        let file_contents: String = fs::read_to_string(path).unwrap();
-        let package_json: Value = from_str(&file_contents).unwrap();
-
-        if !package_json["dependencies"].is_null() {
-            let mut dependencies_map: HashMap<String, Value> = package_json["dependencies"].as_object().unwrap()
-            .iter()
-            .map(|(k, v)| (k.clone(), v.clone()))
-            .collect();
-
-            if !package_json["devDependencies"].is_null() {
-                let dev_dependencies_map: HashMap<String, Value> = package_json["devDependencies"].as_object().unwrap()
-                .iter()
-                .map(|(k, v)| (k.clone(), v.clone()))
-                .collect();
-
-                dependencies_map.extend(dev_dependencies_map);
-            }
-
-            for dependency in &dependencies_map {
-                let dep: &str = dependency.0.as_str();
-                if !helpers::is_dependency_used(&dep, &path.with_file_name("")) && 
-                   !helpers::file_exists_in_node_modules_bin(&dep, &path) {
-                    // delete_dependency(path, dependency);
-                    println!("{} | {}", path.display(), dep)
-                }
-            }
-        }
-    }
+    helpers::scan_directory(&root_path);
 }
